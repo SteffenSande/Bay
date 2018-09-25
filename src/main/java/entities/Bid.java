84e5b12e79910daea87ee64888a51beac9fefa88
Steffen Sande <steffen.sande94@gmail.com>
2018-09-25 12:18:03 +0200

    laget resources

--- src/main/java/entities/Bid.java
+++ src/main/java/entities/Bid.java
@@ -6,18 +6,17 @@ import java.io.Serializable;
 import java.util.Date;
 
 @Entity
-@Table(name="Bid")
 public class Bid implements Serializable
 {
     private static final long serialVersionUID = 1L;
 
     //Create elements ids automatically, incremented 1 by 1
     @TableGenerator(
-            name = "yourTableGenerator",
+            name = "gen",
             allocationSize = 1,
             initialValue = 1)
     @Id
-    @GeneratedValue(strategy=GenerationType.TABLE,generator="yourTableGenerator")
+    @GeneratedValue(strategy=GenerationType.TABLE,generator="gen")
     private int id;
 
     private double value;
