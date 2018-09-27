5f6d38a5a5667b5664eff658b73b150538cdc382
Knut Anders Stokke <knutandersstokke@gmail.com>
2018-09-27 14:01:51 +0200

    Add postgres

--- src/main/java/ejb/startup.java
+++ src/main/java/ejb/startup.java
@@ -7,6 +7,10 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
 
 @Startup
 @Singleton
@@ -15,7 +19,10 @@ public class startup {
     //Ejb hive declaration for this object
 	@EJB
     private AuctionDao bd;
-    
+
+	@PersistenceUnit(unitName="dat250psql")
+    EntityManagerFactory emf;
+
     private Bid bid = new Bid();
     private Feedback feed = new Feedback();
 
@@ -24,7 +31,9 @@ public class startup {
         bid.setValue(1234);
         feed.setContent("Kult produkt");
         feed.setRating(5.6);
-        bd.persist(bid); 	
+
+
+        bd.persist(bid);
         bd.persist(feed);
     }
 }
