c0ec913e4895dc47804372745424323e5befb78f
Knut Anders Stokke <knutandersstokke@gmail.com>
2018-09-27 14:29:39 +0200

    Add simple example of rest method

---
+++ src/main/java/rest/RestTest.java
@@ -0,0 +1,21 @@
+package rest;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by knutandersstokke on 27 27.09.2018.
+ */
+
+@ApplicationScoped
+@Path("/test")
+public class RestTest {
+
+    @GET
+    public Response test() {
+        return Response.ok().build();
+    }
+
+}
