a46814a0871c9158a1df4a9c4c5e8803d206fc08
mathiagr <mathiasgronstad@gmail.com>
2018-10-25 14:54:44 +0200

    Kan nå klikke inn på produkter som er displayet etter kategori og komme inn på produktets auksjon.

--- src/main/java/controllers/productDisplayController.java
+++ src/main/java/controllers/productDisplayController.java
@@ -1,22 +1,45 @@
 package controllers;
 
+import dao.AuctionDao;
 import dao.ProductDao;
 
+import entities.Auction;
 import entities.Product;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.ws.rs.core.Response;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 @Named
 @RequestScoped
-public class productDisplayController {
+public class productDisplayController implements Serializable {
 
     private List<Product> products;
     private String category;
+    private Auction auction;
+    private Product product;
+
+    public Auction getAuction() {
+        return auction;
+    }
+
+    public void setAuction(Auction auction) {
+        this.auction = auction;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
 
     public String getCategory() {
         return category;
@@ -33,7 +56,12 @@ public class productDisplayController {
         this.category = category;
         this.products = productDao.getByCategory(category);
         return "index?faces_redirect=true";
+    }
 
+    public Auction showAuction(int id){
+        this.product = productDao.find(id).get();
+        auction = product.getAuction();
+        return auction;
     }
 
     public List<Product> getProducts() {
